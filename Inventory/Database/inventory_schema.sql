ebcafc6012129c8f4c826dbf57bfe4f73989c41d
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2025-01-08 18:35:22 +0100

    updated schema

--- Inventory/Database/inventory_schema.sql
+++ Inventory/Database/inventory_schema.sql
@@ -2,5 +2,5 @@ CREATE TABLE products (
   id uuid PRIMARY KEY,
   name varchar(100) NOT NULL UNIQUE,
   amount bigint NOT NULL,
-  price money NOT NULL
+  price float8 NOT NULL
 );
---
+++ Inventory/inventoryService/src/main/java/com/example/inventoryService/ProductCreationRecord.java
@@ -0,0 +1,4 @@
+package com.example.inventoryService;
+
+public record ProductCreationRecord() {
+}
