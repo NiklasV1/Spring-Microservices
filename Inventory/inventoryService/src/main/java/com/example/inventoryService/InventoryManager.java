6fdcf0e4586483389c3ec4c75382f395b6c09985
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2025-01-08 16:54:48 +0100

    inventory management class for validation and buisiness logic

--- Inventory/inventoryService/src/main/java/com/example/inventoryService/InventoryManager.java
+++ Inventory/inventoryService/src/main/java/com/example/inventoryService/InventoryManager.java
@@ -1,4 +1,23 @@
 package com.example.inventoryService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class InventoryManager {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<ProductInfo> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        List<ProductInfo> productInfos = new ArrayList<>();
+        for (Product product : products) {
+            productInfos.add(new ProductInfo(product.getId(), product.getName(), product.getAmount(), product.getPrice()));
+        }
+        return productInfos;
+    }
 }
