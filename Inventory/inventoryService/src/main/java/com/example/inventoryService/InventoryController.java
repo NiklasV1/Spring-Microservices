6d44b17b24d4f0e88009c285e30f87222c8ee33b
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2025-01-08 16:55:05 +0100

    Rest Controller for inventory service

--- Inventory/inventoryService/src/main/java/com/example/inventoryService/InventoryController.java
+++ Inventory/inventoryService/src/main/java/com/example/inventoryService/InventoryController.java
@@ -1,4 +1,19 @@
 package com.example.inventoryService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class InventoryController {
+
+    @Autowired
+    private InventoryManager inventoryManager;
+
+    @GetMapping("/getProducts")
+    public List<ProductInfo> getAllProducts() {
+        return inventoryManager.getAllProducts();
+    }
 }
